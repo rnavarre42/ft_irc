83a52add10ffe2a3fafbd1dabd6fb6e33a54cd4e
carruca <tomtomsierra@gmail.com>
2021-12-03 19:20:46 +0100

    Add Client class to src

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,9 +1,22 @@
 #ifndef CLIENT_HPP
 # define CLIENT_HPP
 
-class Client
-{
+# include <string>
+# include <netdb.h>
 
-}
+class	Client
+{
+public:
+	Client(std::string host, std::string port);
+	~Client(void);
+
+private:
+	std::string		_serverHost;
+	std::string		_serverPort;
+	int				_sockfd;
+	struct addrinfo	_hints, *_current, *_first;
+
+	Client(void);
+};
 
 #endif
---
+++ src/Client.cpp
@@ -0,0 +1,8 @@
+#include "Client.hpp"
+#include <string>
+
+Client::Client(std::string _hostname, std::string _port)
+: _
+
+Client::~Client(void)
+{};
